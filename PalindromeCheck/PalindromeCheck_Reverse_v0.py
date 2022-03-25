16782ca101dc0998339dbcf4b26cc75b0ac61d53
YY <2068106+doublewhy@users.noreply.github.com>
2022-03-24 22:58:53 -0400

    Palindrome Check: Reverse Solution (using built-in functionality)

---
+++ PalindromeCheck/PalindromeCheck_Reverse_v0.py
@@ -0,0 +1,8 @@
+# Website: AlgoExpert: Coding Interview Questions
+# Problem: Palindrome Check
+# Difficulty: Easy
+# https://www.algoexpert.io/questions/Palindrome%20Check
+
+
+def isPalindrome(s):
+	return s == s[::-1]
